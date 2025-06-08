298d01fc02f06110998da4831d299c85e62668c4
Abdullah bhatti <bhatti253r@gmail.com>
2025-06-08 14:59:21 +0500

    done

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 app.use(cors({
-  origin: "*", // or specify your frontend URL for security
+  origin: "*", 
 }));
 app.use(express.json());
 
